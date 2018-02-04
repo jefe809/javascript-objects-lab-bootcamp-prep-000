dc9e65d555f585e3240ecd52384912a6428db5fa
Jeffrey Damaso <jeffreydamaso340@gmail.com>
2018-02-04 06:02:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,8 +11,7 @@ function  destructivelyUpdateObjectWithKeyAndValue(object, key, value){
 }
 
 function deleteFromObjectByKey(object, key){
-  var newObj = Object.assign({}, object, key);
-  delete object.key;
+  var newObj = Object.assign({}, object, delete object.key);
   return newObj;
  
 }
