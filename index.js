5402ddee0b5750befd7d9938f56e5b4cd791d543
Jeffrey Damaso <jeffreydamaso340@gmail.com>
2018-02-04 05:19:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,4 +5,7 @@ function updateObjectWithKeyAndValue(object, key, value){
   
 }
 
-function 
+function  destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+  object[key] = value;
+  return object;
+}
